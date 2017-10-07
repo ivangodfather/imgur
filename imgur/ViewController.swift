a8030fa822482dccbebfdf2cbd96136ff34598bb
Ivan Ruiz Monjo <ivanruizmonjo@gmail.com>
2017-10-07 08:39:10 +0200

    Initial Commit

---
+++ imgur/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imgur
+//
+//  Created by Ivan Ruiz Monjo on 07/10/17.
+//  Copyright © 2017 Ivan Ruiz Monjo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
