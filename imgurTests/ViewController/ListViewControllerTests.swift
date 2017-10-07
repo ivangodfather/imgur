bced4f809bd1f32a578ffc3ccb0bb4eb20b70105
Ivan Ruiz Monjo <ivanruizmonjo@gmail.com>
2017-10-07 10:00:29 +0200

    adding xib instead of sb

---
+++ imgurTests/ViewController/ListViewControllerTests.swift
@@ -0,0 +1,27 @@
+//
+//  ListViewController.swift
+//  imgur
+//
+//  Created by Ivan Ruiz Monjo on 07/10/17.
+//  Copyright © 2017 Ivan Ruiz Monjo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+@testable import imgur
+
+class ListViewControllerTests: XCTestCase {
+    
+    var sut: ListViewController!
+    
+    override func setUp() {
+        super.setUp()
+        sut = ListViewController()
+    }
+    
+    func testCollectionViewIsNotNilAfterLoad() {
+        
+    }
+}
+
+
